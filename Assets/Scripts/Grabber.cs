b4c6abba5d2214efac839b8dc40bb8321b64789f
Clayton <claystamper@gmail.com>
2019-08-04 17:44:46 -0500

    cross platform VR input being read

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grabber : MonoBehaviour
-{
+
+
+public class Grabber : MonoBehaviour {
+
+    public Material mat;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +17,23 @@ public class Grabber : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (VRInput.A())
+            print("A");
+        if (VRInput.B())
+            print("B");
+        if (VRInput.X())
+            print("X");
+        if (VRInput.Y())
+            print("Y");
+        if (VRInput.RightThumbClick())
+            print("Right");
+        if (VRInput.LeftThumbClick())
+            print("Left");
         
-        if (OVRInput.Get(OVRInput.Button.Any)) {
-           // Debug.Break();
-        }
+        if (!mat)
+            return;
+        
+        mat.color = Color.Lerp(Color.red, Color.blue, VRInput.RightIndex());
     }
 }
---
+++ Assets/Scripts/VRInput.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRInput : MonoBehaviour {
+   
+    public static float RightIndex() {
+        return Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger");
+    }
+    public static float LeftIndex() {
+        return Input.GetAxis("Oculus_CrossPlatform_PrimaryIndexTrigger");
+    }
+    public static float RightHand() {
+        return Input.GetAxis("Oculus_CrossPlatform_SecondaryHandTrigger");
+    }
+    public static float LeftHand() {
+        return Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger");
+    }
+
+    public static float RightThumbHorizontal() {
+        return Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal");
+    }
+    
+    public static float RightThumbVertical() {
+        return Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical");
+    }
+    
+    public static float LeftThumbHorizontal() {
+        return Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickHorizontal");
+    }
+    
+    public static float LeftThumbVertical() {
+        return Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical");
+    }
+
+    public static bool A() {
+        return Input.GetButton("Oculus_CrossPlatform_A");
+    }
+    
+    public static bool B() {
+        return Input.GetButton("Oculus_CrossPlatform_B");
+    }
+    
+    public static bool X() {
+        return Input.GetButton("Oculus_CrossPlatform_X");
+    }
+    
+    public static bool Y() {
+        return Input.GetButton("Oculus_CrossPlatform_Y");
+    }
+    
+    public static bool RightThumbClick() {
+        return Input.GetButton("Oculus_CrossPlatform_RightThumbClick");
+    }
+    
+    public static bool LeftThumbClick() {
+        return Input.GetButton("Oculus_CrossPlatform_LeftThumbClick");
+    }
+    
+}
+
+
+
+public struct Gesture {
+    private string key;
+}
