c41e6624da4fa1451eecd96a70076415d3e4378a
Clayton <claystamper@gmail.com>
2019-10-18 21:18:52 -0500

    locomotion added

--- Assets/Scripts/Locomotion.cs
+++ Assets/Scripts/Locomotion.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Locomotion : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Locomotion : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
+    [SerializeField]
+    private float  maxSpeed = 2;
+    [SerializeField]
+    private float acceleration = 1;
+    
+    private Vector3 velocity = Vector3.zero;
+    
+    private void Update() {
+
+        Vector3 newVel = Vector3.Lerp(velocity, Vector3.zero, acceleration);
+
+        float x = VRInput.GetAxis(VRButton.LeftThumbHorizontal);
+        float z = VRInput.GetAxis(VRButton.LeftThumbVertical);
+
+        print("X: " + x);
+        print("Z: " + z);
         
+        newVel.x += x;
+        newVel.z += z;
+
+        velocity = Vector3.ClampMagnitude(newVel, maxSpeed);
+        transform.Translate(velocity * Time.deltaTime);
+
     }
 }
