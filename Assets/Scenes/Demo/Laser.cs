888294e78897567b1960fe725c70356211011d32
Clayton <claystamper@gmail.com>
2019-10-21 15:20:46 -0500

    cleaned up

---
+++ Assets/Scenes/Demo/Laser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    public float speed = 10;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+    
+}
