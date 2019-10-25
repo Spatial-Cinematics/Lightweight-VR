45d0484b069c2d4cf9d9fd93427418293d18893d
clayton <claystamper@gmail.com>
2019-10-25 11:29:21 -0500

    cubes spawning

---
+++ Assets/Scripts/Beat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour {
+    [SerializeField] 
+    private MeshRenderer hitBox;
+    
+    public float speed = 1;
+    private void Update() {
+        
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
+        
+    }
+
+    private void Start() {
+        Destroy(gameObject, 5);
+    }
+
+    public void SetMat(Material newMat) {
+        hitBox.material = newMat;
+    }
+    
+}
---
+++ Assets/Scripts/BeatSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSpawner : MonoBehaviour {
+    [SerializeField] private Material blue, red;
+    [SerializeField] 
+    private GameObject beat;
+    [SerializeField]
+    private float spawnWait = 2;
+
+    private float spawnZ = 10;
+    private float spawnX = .5f;
+    private float timeOut = 10f;
+    
+    void Start() {
+        StartCoroutine(LoopSpawn());
+    }
+
+    IEnumerator LoopSpawn() {
+
+        while (true) {
+            
+            yield return new WaitForSeconds(spawnWait);
+            
+            Spawn();
+            
+        }
+        
+    }
+
+    private void Spawn() {
+
+        bool left = Random.value > .5f;
+        Vector3 spawnPos = new Vector3(left ? -spawnX : spawnX, 1, spawnZ);
+        Beat beatInstance = Instantiate(beat, spawnPos, Quaternion.identity).GetComponent<Beat>();
+        Destroy(beatInstance.gameObject, timeOut);
+
+    }
+
+}
